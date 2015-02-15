ff5723083921f1c9368396469a63076c56d79acd
Vivek <viveksck@gmail.com>
2015-02-14 21:23:48 -0500

    0.1.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='changepoint',
-    version='0.1.0',
+    version='0.1.1',
     description='Change point detection in Time series',
     long_description=readme + '\n\n' + history,
     author='Vivek Kulkarni',
